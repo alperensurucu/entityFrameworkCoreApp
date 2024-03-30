5c86a84389c1f5a0a1461e1f8817baab11f6599c
alper <alperensurucu.eng@gmail.com>
2024-03-30 20:06:24 +0300

    added.Builder.services-->DbContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace entityFrameworkCoreApp.Data
+{
+    public class DataContext : DbContext        
+    {
+        // Mesela yeni bir kurs ekleyecek isek
+        // DataContext ten üretilen nesle ile Aşağıdaki datalara erişeceğiz ve
+        // üzerlerine ekleyeceğiz.
+
+        public DbSet<Kurs> Kurslar => Set<Kurs>(); //nullable olabilir kısmı hata vermesin diye initialize ettik.
+        public DbSet<Ogrenci> Ogrenciler => Set<Ogrenci>();
+        public DbSet<KursKayit> KursKayitlari => Set<KursKayit>();
+    }
+}
