1c82ec61a75ae12aca77d7cb09dfa5b8f654b6cd
alper <alperensurucu.eng@gmail.com>
2024-03-31 20:47:25 +0300

    Added.create-Insert-async-injection-metot.

--- Controllers/OgrenciController.cs
+++ Controllers/OgrenciController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using entityFrameworkCoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace entityFrameworkCoreApp.Controllers
 {
     public class OgrenciController : Controller
     {
+        private readonly DataContext _context;
+        public OgrenciController(DataContext context)
+        {
+
+            _context = context;
+
+        }
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogrenci model)
+        {
+            _context.Ogrenciler.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index","Home");
         }
     }
 }
