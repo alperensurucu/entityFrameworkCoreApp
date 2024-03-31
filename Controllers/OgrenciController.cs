711bf5db22c4cb23decf7b02aab52046362d87db
alper <alperensurucu.eng@gmail.com>
2024-03-31 19:29:23 +0300

    Added.Create.cshtml-Ogrenci.Controller-and-_Layout

---
+++ Controllers/OgrenciController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace entityFrameworkCoreApp.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
